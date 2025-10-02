2bfe0f5cd117ea09d8570b4c09df035aa837faa4
algorid <chlwlgns3855@naver.com>
2025-10-02 11:02:40 +0900

    222

--- main_2025_10_02_2.py
+++ main_2025_10_02_2.py
@@ -1,45 +1,68 @@
-# 하나의 변수에 하나의 값을 넣을때
-## 변수이름 = 값
+##변수 : 하나의 값을 저장할 수 있는 공간을 생성하는 것
 
-num = 1
-print(num)
+#1. int(integer) : 정수
+#2. float(float) : 실수
+#3. str(string) : 문자
+#4. bool(bool, boolean) : 참 또는 거짓, true 또는 false, 1 또는 0
+#5. list
+#6. tuple
+#7. dict
+#8. set
 
-#두개의 변수에 하나의 값을 각각 넣을때
-##변수이름1, 변수이름2 = 값1, 값2
+box_num = "하나"
+print(type(box_num))
 
-num_1, num_2 = 1, 2
-print(num_1)
-print(num_2)
-print(num_1, num_2)
+#int, float
+#int -> float
 
-sum = 1+2
-sum = sum + 3
-sum = sum + 4
-sum = sum + 5
+print(float(5))
+print(int(5.0))
+print(dir(10))
 
-print(sum)
+#str : 문자열
+#특징 : 시퀀스 자료형
+#시퀀스란? 요소들이 연속적으로 이어진 자료형
 
-##변수에 값을 넣는 것 => 할당
-## sum = 1 + 2 => 1+2의 계산된 결과를 sum이라는 변수에 할당한다.
-## sum 변수명이고 "=" 같다는 의미가 아니라 할당(assignment)이라고 합니다.
+소속 = "s매직 서울북부"
 
-### "=" : 할당 연산자
-### "+=" : 산술 연산자
-### "+=" : 산술 후 바로 할당하는 문법 ex) /=, -=, *=
+#0 인덱스 시작
+print(소속)
+print(소속[0])
+print(소속[1])
+print(소속[2])
+print(소속[3])
+print(소속[4])
+print(소속[5])
+print(소속[6])
+print(소속[7])
 
-sum2 = 1 + 2
-sum2 += 3
-sum2 += 4
-sum2 += 5
+print("매" in 소속)
 
-## 주의!! 선언되지 않은 변수를 사용할 경우 에러 발생 !! 초기화 필요
-sum3 = 0
-sum3 += 2
+print(len(소속))
+print(소속[3:6])
+print(소속[-1])
 
-#print() 포메팅 3가지 f-string : print(f'{변수명}')
+생년월일 = '2000.12.25'
+년 = 생년월일[0:4]
+월 = 생년월일[5:7]
+일 = 생년월일[8:11]
 
-me = "파머"
-print(f'{me}만 빼고 고양이 다 있어')
+print(f'{년}. {월}. {일}')
 
-answer = 2500 * 2 + 3000 * 3
-print(f'주문이 완료되었습니다. 붕어빵(슈크림) 2개, 붕어빵(잡채) 3개 총 5개 결제 금액은 {answer}원 입니다')
+#전체 출력
+print(생년월일[::])
+
+#전체 출력 리버스
+print(생년월일[::-1])
+
+#첫번째 인덱스 기준으로 2칸씩 띄어쓰기 출력하기
+print(생년월일[::2])
+
+퀴즈 = '스님이 공중에 뜬다를 4글자로 말하면? 어중이떠중이'
+print(퀴즈[22:28])
+
+과일 = 'apple'
+print(dir(과일))
+print(과일.count('p'))
+print(len(과일))
+print(과일.index("p"))
